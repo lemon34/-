1ba1f29b3129bd0b3ea2607d364f11369eef51b0
lemon34 <540476129@qq.com>
2017-11-14 22:14:11 +0800

    Update 1.3.cpp

--- 1.3.cpp
+++ 1.3.cpp
@@ -1,1 +1,48 @@
+/*************************/
+//
+//
+//
+/**********************/
+class CPrefixSorting{
+public: 
+  CPrefixSorting(){
+    m_nCakeCnt = 0;
+    m_nMaxSwap = 0;
+  }
+  ~CPerfixSorting(){
+    if( m_CakeArray != NULL){
+      delete m_CakeArray;
+    }
+    if( m_SwapArray != NULL){
+      delete m_SwapArray;
+    }
+    if( m_ReverseArray != NULL){
+     delete m_ReverseArray;
+   }
+    if( m_ReverseCakeArraySwap != NULL){
+      delete m_ReverseCakeArraySwap;
+    }
+  }
+  //
+  //
+  //
+  //
+  
+  void Run(int* pCakeArray, int nCakeCnt){
+    Init(pCakeArray, nCakeCnt);
+    m_nSearch = 0;
+    Search(0);
+  }
+  void Output(){
+  for(itn i =- 0; i < m_nMaxSwap; i ++){
+    printf("%d", m_SwapArray[i]);
+  }
+  printf("\n |Search Times| : %d\n", m_nSearch);
+  printf("Total Swap times = %d\n", m_nMaxSwap);
+  
+  
+  
+  }
+  
 
+}
